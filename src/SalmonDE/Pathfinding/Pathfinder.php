63ade52d342eb3110fc1a518e39e81288ec0b6e6
SalmonDE <salmonde@salmonde.de>
2019-06-03 20:03:50 +0200

    api 3.x

--- src/SalmonDE/Pathfinding/Pathfinder.php
+++ src/SalmonDE/Pathfinding/Pathfinder.php
@@ -6,6 +6,7 @@ namespace SalmonDE\Pathfinding;
 use pocketmine\block\Block;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\math\Vector3;
 use pocketmine\plugin\PluginBase;
 
 class Pathfinder extends PluginBase {
@@ -29,8 +30,8 @@ class Pathfinder extends PluginBase {
                 return \false;
             }
 
-            $sender->level->setBlockAt($this->pos1->x, $this->pos1->y, $this->pos1->z, Block::get(Block::AIR));
-            $sender->level->setBlockAt($this->pos2->x, $this->pos2->y, $this->pos2->z, Block::get(Block::AIR));
+            $sender->level->setBlock(new Vector3($this->pos1->x, $this->pos1->y, $this->pos1->z), Block::get(Block::AIR));
+            $sender->level->setBlock(new Vector3($this->pos2->x, $this->pos2->y, $this->pos2->z), Block::get(Block::AIR));
 
             $pathfinder = new AStar($sender->level, $this->pos1, $this->pos2);
             $sender->sendMessage('[A*] Calculating ...');
@@ -40,17 +41,19 @@ class Pathfinder extends PluginBase {
             if($node === \null){
                 $sender->sendMessage('[A*] Null.');
             }else{
+                $v = new Vector3();
                 do{
                     if(($node = $node->getPredecessor()) instanceof Node){
-                        $sender->level->setBlockAt($node->x, $node->y, $node->z, Block::get(Block::GOLD_BLOCK));
+                        $v->setComponents($node->x, $node->y, $node->z);
+                        $sender->level->setBlock($v, Block::get(Block::GOLD_BLOCK));
                     }else{
                         break;
                     }
                 }while(\true);
             }
 
-            $sender->level->setBlockAt($this->pos1->x, $this->pos1->y, $this->pos1->z, Block::get(Block::DIAMOND_BLOCK));
-            $sender->level->setBlockAt($this->pos2->x, $this->pos2->y, $this->pos2->z, Block::get(Block::EMERALD_BLOCK));
+            $sender->level->setBlock(new Vector3($this->pos1->x, $this->pos1->y, $this->pos1->z), Block::get(Block::DIAMOND_BLOCK));
+            $sender->level->setBlock(new Vector3($this->pos2->x, $this->pos2->y, $this->pos2->z), Block::get(Block::EMERALD_BLOCK));
         }else{
             return \false;
         }
