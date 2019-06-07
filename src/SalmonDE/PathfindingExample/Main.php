105a21e44017478bbe3ecea7a47c60423851c9d0
SalmonDE <salmonde@salmonde.de>
2019-06-07 22:33:16 +0200

    Add /a* repeat and /a* clear

--- src/SalmonDE/PathfindingExample/Main.php
+++ src/SalmonDE/PathfindingExample/Main.php
@@ -6,18 +6,17 @@ namespace SalmonDE\PathfindingExample;
 use pocketmine\block\Block;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\math\Vector3;
 use pocketmine\plugin\PluginBase;
 use salmonde\pathfinding\Pathfinder;
 
 class Main extends PluginBase {
 
-	private $pos1 = \null;
-	private $pos2 = \null;
+	private $pos1 = null;
+	private $pos2 = null;
 
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $params): bool{
 		if(!isset($params[0])){
-			return \false;
+			return false;
 		}
 
 		if($params[0] === '1'){
@@ -27,34 +26,59 @@ class Main extends PluginBase {
 			$this->pos2 = $sender->floor();
 			$sender->sendMessage('[A*] Pos 2 set');
 		}elseif($params[0] === 'calculate'){
-			if($this->pos1 === \null || $this->pos2 === \null){
-				return \false;
+			if($this->pos1 === null || $this->pos2 === null){
+				return false;
 			}
 
-			$sender->getLevel()->setBlock(new Vector3($this->pos1->x, $this->pos1->y, $this->pos1->z), Block::get(Block::AIR));
-			$sender->getLevel()->setBlock(new Vector3($this->pos2->x, $this->pos2->y, $this->pos2->z), Block::get(Block::AIR));
+			$sender->getLevel()->setBlock($this->pos1, Block::get(Block::AIR));
+			$sender->getLevel()->setBlock($this->pos2, Block::get(Block::AIR));
 
 			$pathfinder = new Pathfinder($sender->getLevel(), $this->pos1, $this->pos2);
+
+			if(isset($params[1])){
+				if(strtolower($params[1]) === '2d'){
+					$pathfinder->getAlgorithm()->set2D();
+				}
+			}
+
 			$sender->sendMessage('[A*] Calculating ...');
 			$pathfinder->findPath();
 			$sender->sendMessage('[A*] Done.');
 
 			$result = $pathfinder->getPathResult();
-			if($result === \null){
+			if($result === null){
 				$sender->sendMessage('[A*] Null.');
 				return true;
 			}else{
 				foreach($result as $block){
-					$sender->getLevel()->setBlock(new Vector3($block->x, $block->y, $block->z), Block::get(Block::GOLD_BLOCK));
+					$sender->getLevel()->setBlock($block, Block::get(Block::GOLD_BLOCK));
 				}
 			}
 
-			$sender->getLevel()->setBlock(new Vector3($this->pos1->x, $this->pos1->y, $this->pos1->z), Block::get(Block::DIAMOND_BLOCK));
-			$sender->getLevel()->setBlock(new Vector3($this->pos2->x, $this->pos2->y, $this->pos2->z), Block::get(Block::EMERALD_BLOCK));
+			$sender->getLevel()->setBlock($this->pos1, Block::get(Block::DIAMOND_BLOCK));
+			$sender->getLevel()->setBlock($this->pos2, Block::get(Block::EMERALD_BLOCK));
+		}elseif($params[0] === 'repeat'){
+			if($this->pos1 === null || $this->pos2 === null){
+				return false;
+			}
+
+			$pathfinder = new Pathfinder($sender->getLevel(), $this->pos1, $this->pos2);
+
+			if(isset($params[1])){
+				if(strtolower($params[1]) === '2d'){
+					$pathfinder->getAlgorithm()->set2D();
+				}
+			}
+
+			$this->getScheduler()->scheduleRepeatingTask(new PathVisualizerTask($pathfinder), 40);
+			$sender->sendMessage('Visualizing ...');
+		}elseif($params[0] === 'clear'){
+			$this->getScheduler()->cancelAllTasks();
+			$sender->sendMessage('[A*] Cleared.');
 		}else{
-			return \false;
+			return false;
 		}
 
-		return \true;
+		return true;
 	}
 }
---
+++ src/SalmonDE/PathfindingExample/PathVisualizerTask.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types = 1);
+
+namespace SalmonDE\PathfindingExample;
+
+use pocketmine\level\particle\DustParticle;
+use pocketmine\level\particle\WaterDripParticle;
+use pocketmine\scheduler\Task;
+use salmonde\pathfinding\Pathfinder;
+use salmonde\pathfinding\PathResult;
+
+class PathVisualizerTask extends Task {
+
+	private $pathfinder;
+
+	public function __construct(Pathfinder $pathfinder){
+		$this->pathfinder = $pathfinder;
+	}
+
+	public function onRun(int $ct): void{
+		$this->pathfinder->getAlgorithm()->resetPathResult();
+		$this->pathfinder->findPath();
+		$path = $this->pathfinder->getPathResult();
+
+		if(!($path instanceof PathResult)){
+			return;
+		}
+
+		$world = $this->pathfinder->getAlgorithm()->getWorld();
+		foreach($path as $pos){
+			$particle = new WaterDripParticle($pos->add(0.5, 0.5, 0.5), 255, 0, 0);
+			$world->addParticle($particle);
+		}
+
+		$particle = new DustParticle($this->pathfinder->getAlgorithm()->getStartPos()->add(0.5, 0.5, 0.5), 0, 0, 255);
+		$world->addParticle($particle);
+		$particle = new DustParticle($this->pathfinder->getAlgorithm()->getTargetPos()->add(0.5, 0.5, 0.5), 0, 255, 0);
+		$world->addParticle($particle);
+	}
+}
