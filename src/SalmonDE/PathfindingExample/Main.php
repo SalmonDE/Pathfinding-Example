54809dda04aa57e73de5e83c42a9b49f06cc6f53
SalmonDE <salmonde@salmonde.de>
2020-08-09 15:21:22 +0200

    clear old paths

--- src/SalmonDE/PathfindingExample/Main.php
+++ src/SalmonDE/PathfindingExample/Main.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 
 namespace SalmonDE\PathfindingExample;
 
+use pocketmine\block\BlockLegacyIds as IDs;
 use pocketmine\block\VanillaBlocks;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
@@ -14,6 +15,9 @@ class Main extends PluginBase {
 
 	private $pos1 = null;
 	private $pos2 = null;
+
+	private $lastPathResult = null;
+	private $lastPathWorld = null;
 
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $params): bool{
 		if(!isset($params[0])){
@@ -35,6 +39,7 @@ class Main extends PluginBase {
 			$sender->getWorld()->setBlock($this->pos2, VanillaBlocks::AIR());
 
 			$pathfinder = new Pathfinder($sender->getWorld(), $this->pos1, $this->pos2);
+			$pathfinder->setMaxJumpHeight(1);
 
 			if(isset($params[1])){
 				if(strtolower($params[1]) === '2d'){
@@ -42,17 +47,23 @@ class Main extends PluginBase {
 				}
 			}
 
+			if($this->lastPathResult !== null){
+				$sender->sendMessage("[A*] Clearing last path ...");
+				$this->clearLastPath();
+			}
+
 			$sender->sendMessage('[A*] Calculating ...');
 			$pathfinder->findPath();
 			$sender->sendMessage('[A*] Done.');
 
-			$result = $pathfinder->getPathResult();
-			if($result === null){
+			$this->lastPathResult = $pathfinder->getPathResult();
+			$this->lastPathWorld = $sender->getWorld();
+			if($this->lastPathResult === null){
 				$sender->sendMessage('[A*] Null.');
 				return true;
 			}else{
-				foreach($result as $block){
-					$sender->getWorld()->setBlock($block, Block::get(Block::GOLD_BLOCK));
+				foreach($this->lastPathResult as $blockPos){
+					$sender->getWorld()->setBlock($blockPos, VanillaBlocks::GOLD());
 				}
 			}
 
@@ -81,5 +92,34 @@ class Main extends PluginBase {
 		}
 
 		return true;
+	}
+
+	public function clearLastPath(): void{
+		if($this->lastPathResult === null){
+			return;
+		}
+
+		foreach($this->lastPathResult as $blockPos){
+			if($this->lastPathWorld->getBlock($blockPos)->getId() === Ids::GOLD_BLOCK){
+				$this->lastPathWorld->setBlock($blockPos, VanillaBlocks::AIR());
+			}
+		}
+
+		$this->lastPathResult = null;
+		$this->lastPathWorld = null;
+
+		if($this->lastPathWorld->getBlock($this->pos1)->getId() === Ids::DIAMOND_BLOCK){
+			$this->lastPathWorld->setBlock($this->pos1, VanillaBlocks::AIR());
+		}
+
+		if($this->lastPathWorld->getBlock($this->pos2)->getId() === Ids::EMERALD_BLOCK){
+			$this->lastPathWorld->setBlock($this->pos2, VanillaBlocks::AIR());
+		}
+	}
+
+	public function onDisable(): void{
+		if($this->lastPathResult !== null){
+			$this->clearLastPath();
+		}
 	}
 }
